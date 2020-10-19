bfb38c8b64228fcfda6d4c05ae883d16ebd0e6e1
Nick Massie <45523540+nimas0@users.noreply.github.com>
2020-10-19 11:45:40 -0700

    remove firebase stuff for countdown login

--- components/LoginModal.jsx
+++ components/LoginModal.jsx
@@ -50,45 +50,44 @@ export default ({ shown, setShown }) => {
    const [newUser, setNewUser] = useState(undefined);
 
    // Firebase auth config
-   const firebaseAuthConfig = {
-      signInFlow: 'popup',
-      // Auth providers
-      signInOptions: [
-         {
-            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            requireDisplayName: false,
-         },
-         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-         {
-            provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            scopes: ['public_profile', 'email'],
-         },
-      ],
-      credentialHelper: 'none',
-      callbacks: {
-         signInSuccessWithAuthResult: (authResult) => {
-            setUserId(authResult.user.uid);
-            processUser(authResult, (isNew, verified) => {
-               console.log(verified);
-               setNewUser(isNew);
-               if (verified) {
-                  // (fulfills on true or "pending")
-                  // Login complete
-                  close();
-               } else {
-                  // Go to verification
-                  setPage('verification');
-               }
-            });
-            setPage('authWait');
-            return false; // Prevents redirect
-         },
-      },
-   };
+   // const firebaseAuthConfig = {
+   //    signInFlow: 'popup',
+   //    // Auth providers
+   //    signInOptions: [
+   //       {
+   //          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+   //          requireDisplayName: false,
+   //       },
+   //       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+   //       {
+   //          provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+   //          scopes: ['public_profile', 'email'],
+   //       },
+   //    ],
+   //    credentialHelper: 'none',
+   //    callbacks: {
+   //       signInSuccessWithAuthResult: (authResult) => {
+   //          setUserId(authResult.user.uid);
+   //          processUser(authResult, (isNew, verified) => {
+   //             console.log(verified);
+   //             setNewUser(isNew);
+   //             if (verified) {
+   //                // (fulfills on true or "pending")
+   //                // Login complete
+   //                close();
+   //             } else {
+   //                // Go to verification
+   //                setPage('verification');
+   //             }
+   //          });
+   //          setPage('authWait');
+   //          return false; // Prevents redirect
+   //       },
+   //    },
+   // };
 
    return (
       <Modal style={{color: '#ffff', opacity: 1}} className='border-0 m-4 '  centered show={shown} onHide={close} backdrop={uploading ? 'static' : true}>
-         {page === 'auth' ? (
             <>
                <Modal.Header  className='text-white bg-dark schedulingShadow' closeButton>
                   <Modal.Title  className=' text-center '>Full Website Coming Soon</Modal.Title>
@@ -110,206 +109,88 @@ export default ({ shown, setShown }) => {
                   <Button size='large' variant='outlined' className='text-primary ' onClick={() => router.push('/learnmore')}>LEARN MORE</Button>
                </Modal.Body>
             </>
-         ) : page === 'authWait' ? (
-            <>
-               <Modal.Header closeButton>
-                  <Modal.Title>Please Sign In</Modal.Title>
-               </Modal.Header>
-
-               <Modal.Body className='text-center'>
-                  <Spinner animation='border' variant='primary' />
-               </Modal.Body>
-            </>
-         ) : page === 'verification' ? (
-            <>
-               <Modal.Header>
-                  <Modal.Title>Upload Pre-Approval</Modal.Title>
-                  {/* Using custom button so I can disable it */}
-                  <button
-                     type='button'
-                     className='close'
-                     onClick={close}
-                     aria-label='Close'
-                     disabled={uploading}>
-                     <span aria-hidden='true'>&times;</span>
-                  </button>
-               </Modal.Header>
-
-               <Modal.Body>
-                  <p>
-                     You need to upload a verified pre-approval or proof of funds to book a tour
-                     with a home. You may skip this process for now.
-                  </p>
-                  <Form.Control
-                     ref={fileInput}
-                     type='file'
-                     name='document'
-                     aria-label='Verification document'
-                     onChange={(e) => setFileSelected(!!e.target.files)}
-                     disabled={uploading}
-                  />
-                  {uploadFailure && (
-                     <Form.Control.Feedback type='invalid'>
-                        We're experiencing network errors&mdash;please try again later.
-                     </Form.Control.Feedback>
-                  )}
-               </Modal.Body>
-
-               <Modal.Footer>
-                  <Button
-                     variant='secondary'
-                     onClick={() => (newUser ? setPage('welcome') : close())}
-                     disabled={uploading}>
-                     Skip
-                  </Button>
-                  <Button
-                     variant='primary'
-                     onClick={uploadVerification}
-                     disabled={!fileSelected || uploading}>
-                     {uploading ? (
-                        <>
-                           <Spinner
-                              as='span'
-                              animation='border'
-                              size='sm'
-                              role='status'
-                              aria-hidden='true'
-                              className='position-relative mr-2'
-                              style={{ bottom: 3 }}
-                           />
-                           Uploading...
-                        </>
-                     ) : (
-                        'Upload'
-                     )}
-                  </Button>
-               </Modal.Footer>
-            </>
-         ) : page === 'welcome' ? (
-            <>
-               <Modal.Header closeButton>
-                  <Modal.Title>Welcome!</Modal.Title>
-               </Modal.Header>
-
-               <Modal.Body>
-                  <p>
-                     You have successfully created an account. If you did not upload a pre-approval
-                     you may still secure a time and date with this home. If you still have not
-                     uploaded a pre-approval within 12 hours of the appointment, the appointment
-                     will automatically be canceled.
-                  </p>
-               </Modal.Body>
-
-               <Modal.Footer>
-                  <Button variant='secondary' onClick={close}>
-                     Return to listing
-                  </Button>
-                  <Button variant='primary' href='#'>
-                     {/* TODO: Add buyer dashboard link */}
-                     Go to buyer dashboard
-                  </Button>
-               </Modal.Footer>
-            </>
-         ) : (
-            // page === "uploaded"
-            <>
-               <Modal.Header closeButton>
-                  <Modal.Title>Upload complete!</Modal.Title>
-               </Modal.Header>
-
-               <Modal.Footer>
-                  <Button variant='secondary' onClick={close}>
-                     Return to listing
-                  </Button>
-                  <Button variant='primary' href='#'>
-                     {/* TODO: Add buyer dashboard link */}
-                     Go to buyer dashboard
-                  </Button>
-               </Modal.Footer>
-            </>
-         )}
       </Modal>
    );
 
-   async function processUser(authResult, callback) {
-      const { uid: id, displayName, photoURL } = authResult.user;
+   // async function processUser(authResult, callback) {
+   //    const { uid: id, displayName, photoURL } = authResult.user;
 
-      // Get or create user in Firestore
-      try {
-         const response = await fetch('/api/check-user', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id, displayName, photoURL }),
-         });
+   //    // Get or create user in Firestore
+   //    try {
+   //       const response = await fetch('/api/check-user', {
+   //          method: 'POST',
+   //          headers: { 'Content-Type': 'application/json' },
+   //          body: JSON.stringify({ id, displayName, photoURL }),
+   //       });
 
-         if (response.ok) {
-            // Return whether this user is new and user's verification status
-            const { created: isNew, verification } = await response.json();
-            const { status: verified } = verification;
-            return callback(isNew, verified);
-         } else {
-            // https://github.com/developit/unfetch#caveats
-            let error = new Error(response.statusText);
-            error.response = response;
-            throw error;
-         }
-      } catch (err) {
-         console.error(
-            'Auth success but database failure. Either a coding error or network issues.',
-            err
-         );
-         close();
-      }
-   }
+   //       if (response.ok) {
+   //          // Return whether this user is new and user's verification status
+   //          const { created: isNew, verification } = await response.json();
+   //          const { status: verified } = verification;
+   //          return callback(isNew, verified);
+   //       } else {
+   //          // https://github.com/developit/unfetch#caveats
+   //          let error = new Error(response.statusText);
+   //          error.response = response;
+   //          throw error;
+   //       }
+   //    } catch (err) {
+   //       console.error(
+   //          'Auth success but database failure. Either a coding error or network issues.',
+   //          err
+   //       );
+   //       close();
+   //    }
+   // }
 
-   async function uploadVerification() {
-      // Set loading animations
-      setUploading(true);
+   // async function uploadVerification() {
+   //    // Set loading animations
+   //    setUploading(true);
 
-      // Remove upload failure message if there
-      setUploadFailure(false);
+   //    // Remove upload failure message if there
+   //    setUploadFailure(false);
 
-      if (fileInput.current.files[0]) {
-         try {
-            // Upload file to Cloud Storage
-            const documentURL = await uploadUserDocument(
-               fileInput.current.files[0],
-               `users/${userId}`
-            );
+   //    if (fileInput.current.files[0]) {
+   //       try {
+   //          // Upload file to Cloud Storage
+   //          const documentURL = await uploadUserDocument(
+   //             fileInput.current.files[0],
+   //             `users/${userId}`
+   //          );
 
-            // Send file info through API
-            const response = await fetch('/api/verification-doc', {
-               method: 'POST',
-               headers: { 'Content-Type': 'application/json' },
-               body: JSON.stringify({ documentURL, userId }),
-            });
+   //          // Send file info through API
+   //          const response = await fetch('/api/verification-doc', {
+   //             method: 'POST',
+   //             headers: { 'Content-Type': 'application/json' },
+   //             body: JSON.stringify({ documentURL, userId }),
+   //          });
 
-            if (response.ok) {
-               // Move on
-               if (newUser) {
-                  setPage('welcome');
-               } else {
-                  setPage('uploaded');
-               }
-            } else {
-               // https://github.com/developit/unfetch#caveats
-               let error = new Error(response.statusText);
-               error.response = response;
-               throw error;
-            }
-         } catch (err) {
-            // Add upload failure message
-            console.error('Either a coding error or network issues', err);
-            setUploadFailure(true);
-            setUploading(false);
-         }
-      } else {
-         // Treat it as skipping the file upload
-         if (newUser) {
-            setPage('welcome');
-         } else {
-            close();
-         }
-      }
-   }
+   //          if (response.ok) {
+   //             // Move on
+   //             if (newUser) {
+   //                setPage('welcome');
+   //             } else {
+   //                setPage('uploaded');
+   //             }
+   //          } else {
+   //             // https://github.com/developit/unfetch#caveats
+   //             let error = new Error(response.statusText);
+   //             error.response = response;
+   //             throw error;
+   //          }
+   //       } catch (err) {
+   //          // Add upload failure message
+   //          console.error('Either a coding error or network issues', err);
+   //          setUploadFailure(true);
+   //          setUploading(false);
+   //       }
+   //    } else {
+   //       // Treat it as skipping the file upload
+   //       if (newUser) {
+   //          setPage('welcome');
+   //       } else {
+   //          close();
+   //       }
+   //    }
+   // }
 };
