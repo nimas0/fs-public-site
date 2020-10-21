dc892a43e603272972e7b0969cc00ea8b25fc6e6
Nick Massie <45523540+nimas0@users.noreply.github.com>
2020-10-21 09:12:29 -0700

    asldfjasldkfj

--- utils/firebaseInit.js
+++ utils/firebaseInit.js
@@ -1,19 +1,25 @@
-"use strict";
+import firebase, { database } from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/database';
+import 'firebase/storage';
 
-import firebase from "firebase/app";
-
-export default () => {
-  // Initialize Firebase if it hasn't been already
-  if (!firebase.apps.length) {
-    firebase.initializeApp({
-      apiKey: process.env.API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      databaseURL: process.env.DATABASE_URL,
-      projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID,
-      measurementId: process.env.MEASUREMENT_ID
-    });
-  }
+// Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyBDtdJhJUuHUGEFW-ViyDxJErx2JoFz8Jw",
+  authDomain: "finding-spaces-73b23.firebaseapp.com",
+  databaseURL: "https://finding-spaces-73b23.firebaseio.com",
+  projectId: "finding-spaces-73b23",
+  storageBucket: "finding-spaces-73b23.appspot.com",
+  messagingSenderId: "48951865601",
+  appId: "1:48951865601:web:2eb4d8a3fe14a1caf22b00",
+  measurementId: "G-7CK0W02CGV"
 };
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+firebase.firestore();
+
+export default firebase;
