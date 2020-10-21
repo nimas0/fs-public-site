545b905c6f961f7ab847b3eecd04ab7697936e48
Nick Massie <45523540+nimas0@users.noreply.github.com>
2020-10-21 13:28:10 -0700

    2

---
+++ pages/api/test-me.js
@@ -0,0 +1,20 @@
+"use strict";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import firebaseInit from "../../utils/firebaseInit";
+import uid from 'uid';
+import { DateTime } from 'luxon'
+
+firebaseInit();
+// Submit propsal for informal offer to interested collection in database
+/// POST only
+export default async (req, res) => {
+
+
+
+
+    console.log('the api connected!')
+
+
+}
