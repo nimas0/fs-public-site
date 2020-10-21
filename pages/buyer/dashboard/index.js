e08e3b9e8221bd4b10b7794cc4ccd320d383c717
Nick Massie <45523540+nimas0@users.noreply.github.com>
2020-10-21 12:16:35 -0700

    2

--- pages/buyer/dashboard/index.js
+++ pages/buyer/dashboard/index.js
@@ -104,38 +104,38 @@ Dashboard.getInitialProps = async (ctx) => {
     const userId = ctx.myCustomData.AuthUserInfo.AuthUser.id;
 
     // Get profile data
-    const userProfileFetch = fetch(`${process.env.HOST}/api/user?id=${userId}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    });
+//     const userProfileFetch = fetch(`${process.env.HOST}/api/user?id=${userId}`, {
+//         method: 'GET',
+//         headers: { 'Content-Type': 'application/json' },
+//     });
 
 
-    try {
-        const [userProfileResponse] = await Promise.all([userProfileFetch]);
+//     try {
+//         const [userProfileResponse] = await Promise.all([userProfileFetch]);
 
-        if (userProfileResponse.ok) {
-            const { verification } = await userProfileResponse.json();
-            console.log(verification);
-            return {
-                verification: verification
-            }
-        } else if ([404, 503].includes(userProfileResponse.status)) {
-            return { statusCode: userProfileResponse.status };
-        } else {
-            // https://github.com/developit/unfetch#caveats
-            let error = new Error(userProfileResponse.statusText);
-            error.response = userProfileResponse;
-            throw error;
-        }
+//         if (userProfileResponse.ok) {
+//             const { verification } = await userProfileResponse.json();
+//             console.log(verification);
+//             return {
+//                 verification: verification
+//             }
+//         } else if ([404, 503].includes(userProfileResponse.status)) {
+//             return { statusCode: userProfileResponse.status };
+//         } else {
+//             // https://github.com/developit/unfetch#caveats
+//             let error = new Error(userProfileResponse.statusText);
+//             error.response = userProfileResponse;
+//             throw error;
+//         }
 
 
 
-    } catch (err) {
-        console.log(err)
-        return {
-            statusCode: (err.response && err.response.status) || err.statusCode || 500,
-        };
-    }
-}
+//     } catch (err) {
+//         console.log(err)
+//         return {
+//             statusCode: (err.response && err.response.status) || err.statusCode || 500,
+//         };
+//     }
+// }
 
 export default withAuthUser(withAuthUserInfo(withLoginModal(Dashboard)));
