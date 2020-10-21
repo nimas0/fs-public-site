7656cba59eca869efcc9719d2f942fc166affcce
Nick Massie <45523540+nimas0@users.noreply.github.com>
2020-10-21 15:21:47 -0700

    2

--- pages/api/user.js
+++ pages/api/user.js
@@ -11,31 +11,30 @@ export default async (req, res) => {
   const { id } = req.query;
 
   console.log('id', id)
-  res.status(200).json({ id });
-  // // Set up database connection
-  // const db = firebase.firestore();
+  // Set up database connection
+  const db = firebase.firestore();
 
-  // // Get current user data
-  // try {
-  //   const user = await db
-  //     .collection("users")
-  //     .doc(id)
-  //     .get();
-  //   if (user.exists) {
-  //     // 200 OK
-  //     res.status(200).json({ id, ...user.data() });
-  //   } else {
-  //     // 404 Not Found
-  //     res.status(404).json({ message: "No user found" });
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   const { response } = err;
-  //   if (response) {
-  //     res.status(response.status).json({ message: response.statusText });
-  //   } else {
-  //     // 503 Service Unavailable
-  //     res.status(503).json({ message: err.message });
-  //   }
-  // }
+  // Get current user data
+  try {
+    const user = await db
+      .collection("users")
+      .doc(id)
+      .get();
+    if (user.exists) {
+      // 200 OK
+      res.status(200).json({ id, ...user.data() });
+    } else {
+      // 404 Not Found
+      res.status(404).json({ message: "No user found" });
+    }
+  } catch (err) {
+    console.log(err);
+    const { response } = err;
+    if (response) {
+      res.status(response.status).json({ message: response.statusText });
+    } else {
+      // 503 Service Unavailable
+      res.status(503).json({ message: err.message });
+    }
+  }
 };
