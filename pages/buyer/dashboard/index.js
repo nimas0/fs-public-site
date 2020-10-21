6e53a6de48df348ff459c55b68f2f70b0f2e4438
Nick Massie <45523540+nimas0@users.noreply.github.com>
2020-10-21 13:05:55 -0700

    2

--- pages/buyer/dashboard/index.js
+++ pages/buyer/dashboard/index.js
@@ -32,7 +32,7 @@ const Dashboard = ({ AuthUserInfo, showLoginModal, verification, subscriptionDat
             .collection('users')
             .doc(AuthUser.id)
     );
-    console.log(window.location.host)
+    console.log('sdlfjsdlfj')
     // console.log(process.env.HOST)
 
     //TODO : clean up return. getting messy with error && error statments
