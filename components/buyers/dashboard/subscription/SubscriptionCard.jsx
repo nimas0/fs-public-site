6cb311c2a510f2115c48b67983a130f9da4449f1
Nick Massie <45523540+nimas0@users.noreply.github.com>
2020-11-12 13:40:37 -0700

    accepted offer content

--- components/buyers/dashboard/subscription/SubscriptionCard.jsx
+++ components/buyers/dashboard/subscription/SubscriptionCard.jsx
@@ -5,6 +5,7 @@ import Details from './Details';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHamburger, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from 'next/router';
+import GenericModal from '../../../GenericModal';
 
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
    <a
@@ -71,8 +72,43 @@ const handleUnsubscribe = async (interestId) => {
 const SubscriptionCard = ({ subscriptionData, verification, interestId }) => {
    const [key, setKey] = useState('action');
    const router = useRouter();
+   const [modalShow, setModalShow] = React.useState(false);
+
+
+   const instructions = () => (
+      <ListGroup>
+     <p className='mb-5'>Congratulations! You and the seller have agreed on the essential terms of the offer. Below are the steps needed to complete the remainder of the process and get the keys to your new home!</p>
+     <Button onClick={() => setModalShow(true)}>See Next Steps</Button>
+   </ListGroup>
+   )
+   
+   const ModalBody = () => (
+      <>
+       <ListGroup>
+     <h6 className='mb-5 mt-2 p-2'><b>      Congratulations!</b> You and the seller have agreed on the essential terms of the offer. Below are the steps needed to complete the remainder of the process and get the keys to your new home!</h6>
+     <ListGroup.Item>
+        <h5>
+       <b> 1. Acquire a purchase agreement</b>
+         </h5> 
+         Now, since you and the seller have agreed to the essential terms, it's time to make it official. A purchase agreement is a document (required by law) that states all terms and timelines of a real estate transaction. Download our free purchase agreement contract. It has been drafted by and reviewed by attorneys in Arizona and meets all the binding contract requirements.
+      <Button variant='outlined' size='lg' className='text-primary'><h6 className='pt-3 text-primary'><b>Download Purchase Agreement</b></h6></Button>
+   </ListGroup.Item>
+     <ListGroup.Item><h5><b>2. Fill out purchase agreement</b></h5> Fill out the purchase agreement according to the terms and conditions you and the seller agreed on. The contract will have more information to fill out; make sure to read thoroughly and contact a lawyer if you run into any complications. 
+   </ListGroup.Item>
+     <ListGroup.Item><h5><b>3. Send to Seller</b></h5> After completion of the purchase agreement, the seller will need to review and sign. We recommend that you upload your document directly to the seller through the Finding Spaces dashboard; This will allow you and the seller to communicate and hash out terms quickly.</ListGroup.Item>
+     <ListGroup.Item><h5><b>4. Notify Title Company</b></h5> Next, deliver the completed and signed contract with the agreed amount of earnest deposit to the title company stated in the agreement. You and the seller can negotiate who hires a title company and pays associated fees. The title company will order the title, property tax information, loan balances, and other necessary paperwork. You can typically use your title company as an escrow agent at no extra charge. The escrow agent will also serve as a third party who holds money in a trust until a property sale closes.</ListGroup.Item>
+     <ListGroup.Item><h5><b>5. Contengencies</b></h5> It's your responsibility to remove all contingencies in the proper timelines stated in the contract. The Finding Spaces dashboard will remain available for you to chat, schedule appointments, and exchange documents with the seller. </ListGroup.Item>
+     <ListGroup.Item><h5><b>6. Home Sold</b></h5> Once all contingencies have been uplifted, it will be time to close the deal. The title company will provide you with the documents you need for the closing process. After the documents are signed and recorded and the funds transferred to the seller, you will receive the keys to your new home!</ListGroup.Item>
+
+   </ListGroup>
+      </>
+   );
+
+
+   
    console.log('subscriptionData', interestId);
    return (
+      <>
       <Row>
          <Col>
             <Card  className='buttonShadow border-0 bg-white mb-4'>
@@ -184,17 +220,16 @@ const SubscriptionCard = ({ subscriptionData, verification, interestId }) => {
             </Card>
          </Col>
       </Row>
+              <GenericModal
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+              header='Informal Offer Accepted'
+              body={<ModalBody />}
+           />
+           </>
    );
 };
 
-const instructions = () => (
-   <ListGroup>
-  <h6>Seller is drafting up a contract and will be email it to you shortly.</h6>
-  <p>Review these resources while you are waiting to prepare yourself.</p>
-  <ListGroup.Item>What next?</ListGroup.Item>
-  <ListGroup.Item>Other Idea</ListGroup.Item>
-  <ListGroup.Item>Negotiations</ListGroup.Item>
-</ListGroup>
-)
+
 
 export default SubscriptionCard;
