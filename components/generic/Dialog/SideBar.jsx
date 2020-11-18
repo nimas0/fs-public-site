ba8170bb548db2eb0fa99eed1fdceb3504d4ba76
Nick Massie <45523540+nimas0@users.noreply.github.com>
2020-11-17 21:20:00 -0700

    fix sidebar attempt 3

--- components/generic/Dialog/SideBar.jsx
+++ components/generic/Dialog/SideBar.jsx
@@ -2,33 +2,33 @@ import React from 'react';
 import { Col, Row, ListGroup } from 'react-bootstrap';
 
 const SideBar = ({ sidebarHeader, subHeaderText, enabled = false, links }) => {
-   const renderedComponent = () =>
-      enabled ? (
-         <Col xs='4' className='border-left'>
-            <div className='p-4'>
-               <Row className='pb-3'>
-                  <h6>
-                     <b>{sidebarHeader}</b>
-                  </h6>
-               </Row>
-               <Row>
-                     <small>{subHeaderText}</small>
-               </Row>
-            </div>
-            <ListGroup variant='flush' className='d-flex h-100'>
-               {/* {links &&
-                  links.map((link, index) => {
-                     return (
-                        <ListGroup.Item key={index} className='py-2 pl-2 pr-2'>
-                           <a className='text-success' href={link.url}>{link.title}</a>
-                        </ListGroup.Item>
-                     );
-                  })} */}
-            </ListGroup>
-         </Col>
-      ) : '';
+   // const renderedComponent = () =>
+   //    enabled ? (
+   //       <Col xs='4' className='border-left'>
+   //          <div className='p-4'>
+   //             <Row className='pb-3'>
+   //                <h6>
+   //                   <b>{sidebarHeader}</b>
+   //                </h6>
+   //             </Row>
+   //             <Row>
+   //                   <small>{subHeaderText}</small>
+   //             </Row>
+   //          </div>
+   //          <ListGroup variant='flush' className='d-flex h-100'>
+   //             {/* {links &&
+   //                links.map((link, index) => {
+   //                   return (
+   //                      <ListGroup.Item key={index} className='py-2 pl-2 pr-2'>
+   //                         <a className='text-success' href={link.url}>{link.title}</a>
+   //                      </ListGroup.Item>
+   //                   );
+   //                })} */}
+   //          </ListGroup>
+   //       </Col>
+   //    ) : '';
 
-   return 'test';
+   return <p>test</p>;
 };
 
 export default SideBar;
