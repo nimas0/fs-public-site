fc9d34cdda1f913889b01d4af906a8a0e96f7c8a
Nick Massie <45523540+nimas0@users.noreply.github.com>
2021-01-15 14:08:46 -0700

    merge fix

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import Nav from '../Nav';
-import MainNav from '../navigation/MainNav';
-import Head from 'next/head'
-
+import React from "react";
+import Nav from "../Nav";
+import MainNav from "../navigation/MainNav";
+import Head from "next/head";
 
 const MainLayout = ({ AuthUser, showLoginModal, children }) => (
   <>
     <Head>
       <body />
     </Head>
-    <MainNav search={true} AuthUser={AuthUser} showLoginModal={showLoginModal} />
+    <MainNav
+      search={true}
+      AuthUser={AuthUser}
+      showLoginModal={showLoginModal}
+    />
     {children}
-    
   </>
-)
+);
 
 export default MainLayout;
