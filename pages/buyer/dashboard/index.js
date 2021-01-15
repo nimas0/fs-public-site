18895c8abb061477d3aef3379ea4667534643cea
Nick Massie <45523540+nimas0@users.noreply.github.com>
2021-01-15 14:12:19 -0700

    fix again

--- pages/buyer/dashboard/index.js
+++ pages/buyer/dashboard/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 import fetch from "isomorphic-unfetch";
 import MainLayout from "../../../components/layout/MainLayout";
@@ -21,35 +20,10 @@ import Nav from "../../../components/Nav";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
-=======
-import React, { useEffect } from 'react';
-import fetch from 'isomorphic-unfetch';
-import { Container, Row, Col, Button, Card } from 'react-bootstrap';
-import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
-import firebase from 'firebase/app';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { useRouter } from 'next/router';
-import MainLayout from '../../../components/layout/MainLayout';
-import Heading from '../../../components/buyers/dashboard/Heading';
-import Approval from '../../../components/buyers/dashboard/approval/Approval';
-import SubscriptionCard from '../../../components/buyers/dashboard/subscription/SubscriptionCard';
-import Resources from '../../../components/buyers/dashboard/Resources';
-import withAuthUser from '../../../utils/pageWrappers/withAuthUser';
-import withAuthUserInfo from '../../../utils/pageWrappers/withAuthUserInfo';
-import withLoginModal from '../../../utils/pageWrappers/withLoginModal';
-
-import SellerSignUp from '../../../components/buyers/dashboard/sellersignup/SellerSignUp'
-
-import firebaseInit from '../../../utils/firebaseInit';
-import "firebase/firestore";
-import Nav from '../../../components/Nav';
->>>>>>> dev-main
 
 // Initialize Firebase ap
 firebaseInit();
 
-<<<<<<< HEAD
 const Dashboard = ({
   AuthUserInfo,
   showLoginModal,
@@ -119,60 +93,6 @@ const Dashboard = ({
                                        Search Using Home Code`
                                        </Card.Header>  */}
                   {/* <Card.Body className='text-center py-5'>
-=======
-
-const Dashboard = ({ AuthUserInfo, showLoginModal, verification, subscriptionData }) => {
-    const router = useRouter();
-    const { AuthUser = null } = AuthUserInfo;
-    const [value, loading, error] = useCollection(firebase.firestore().collection('interest').where('buyer.buyerUid', '==', AuthUser.id));
-    const [userDoc, loadingUserDoc, errorUserDoc] = useDocument(
-        firebase
-            .firestore()
-            .collection('users')
-            .doc(AuthUser.id)
-    );
-
-
-
-    // TODO : clean up return. getting messy with error && error statments
-
-    return (
-      <>
-        <Nav showLogo showQuickLinks={false} AuthUser={AuthUser} showLoginModal={showLoginModal} />
-        <Container>
-          <Row>
-            <Col xs='6'>
-              <Heading AuthUser={AuthUser} />
-              <Row className='pb-1 mx-1'>
-                {/* {/* <Card.Header className='bg-transparent  border-0 ' as='h4'>
-                                Seller's Dashboard
-                                       </Card.Header> */}
-                {/* <Button href={!loadingUserDoc && userDoc.data().defaultListingId ? 'http://localhost:3001/showings' : '/learnmore'} className=' rounded-lg mx-3' variant="primary" size="md" block>
-                                {!loadingUserDoc && userDoc.data().defaultListingId ? 'Go to Seller\'s Dashboard' : 'Learn More: Sell Your Home Free'}
-                            </Button> */}
-
-              </Row>
-              {
-                            errorUserDoc ? <strong>Error: {JSON.stringify(error)}</strong> :
-                            <Approval key={userDoc} verification={loadingUserDoc ? verification : userDoc.data().verification} AuthUser={AuthUser} />
-                        }
-
-
-
-              {error && errorUserDoc && <strong>Error: {JSON.stringify(error)}</strong>}
-              <Resources />
-
-            </Col>
-            <Col xs={{ span: 6 }}>
-
-              <Row>
-                <Col>
-                  <Card className='border-0 mb-2 bg-transparent'>
-                    {/* <Card.Header className='bg-transparent py-4  border-0 ' as='h4'>
-                                       Search Using Home Code`
-                                       </Card.Header>  */}
-                    {/* <Card.Body className='text-center py-5'>
->>>>>>> dev-main
 
                                         <Card.Text>
                                             <small>
@@ -180,7 +100,6 @@ const Dashboard = ({ AuthUserInfo, showLoginModal, verification, subscriptionDat
                                             </small>
                                         </Card.Text>
                                     </Card.Body> */}
-<<<<<<< HEAD
                 </Card>
               </Col>
             </Row>
@@ -263,88 +182,9 @@ const Dashboard = ({ AuthUserInfo, showLoginModal, verification, subscriptionDat
       </Container>
     </>
   );
-=======
-                  </Card>
-
-                </Col>
-              </Row>
-              <p className='text-muted'>Quick links</p>
-              {/* <SellerSignUp key={userDoc} verification={loadingUserDoc ? verification : userDoc.data().verification} AuthUser={AuthUser} /> */}
-              <Row className='mb-5'>
-
-                <Col xs={12}>
-                  <Button
-                    href={userDoc && (userDoc.data()).hasOwnProperty('defaultListingId') ?  'https://seller.findingspaces.com/showings' :  'https://findingspaces.com/learnmore'}
-                                    // onClick={userDoc && (userDoc.data()).hasOwnProperty('defaultListingId') && router.push('/')}
-                    variant='primary'
-                    style={{ borderStyle: 'solid', borderRadius: 10 }}
-                    block
-                    className=' p-3 text-white m-1 buttonShadow  d-flex justify-content-between border-rounded  '
-                  >
-                    {userDoc && (userDoc.data()).hasOwnProperty('defaultListingId') ? 'Go to Seller\'\s dashboard' : 'Sell Your Home Without An Agent'}
-                    <b><FontAwesomeIcon icon={faChevronRight} color='white' /></b>
-                  </Button>
-
-                </Col>
-                <Col>
-                  <Button
-                    onClick={() => router.push('/')}
-                    style={{ borderStyle: 'solid', borderRadius: 10 }}
-                    block
-                    className=' p-3 text-white m-1 buttonShadow  d-flex justify-content-between border-rounded  '
-                  >
-                    Enter Home Code
-                    <b><FontAwesomeIcon icon={faChevronRight} color='white' /></b>
-                  </Button>
-                </Col>
-              </Row>
-              <p className='text-muted'>Subscriptions</p>
-              {loading && loadingUserDoc && <span>Loading...</span>}
-              {(value && userDoc) && (
-                <span>
-                  {
-                                    value.docs.length === 0 ?
-                                        (
-                                          <Card style={{ height: '20rem' }} className='border rounded'>
-                                            <Card.Header>You are not currently subscribed to any property.</Card.Header>
-                                            <Card.Body>
-                                              <h6>Once subscribed you can: </h6>
-                                              <ul>
-                                                <li>Make offers</li>
-                                                <li>Chat with Homeowner</li>
-                                                <li>Schedule Showings</li>
-                                                <li>Recieve updates</li>
-                                              </ul>
-                                            </Card.Body>
-                                          </Card>
-                                        )
-                                        :
-                                        (
-
-                                            value.docs.map(doc => (
-                                              <React.Fragment key={doc.id}>
-                                                <SubscriptionCard interestId={doc.id} verification={loadingUserDoc ? verification : userDoc.data().verification} subscriptionData={doc.data()} />
-                                              </React.Fragment>
-                                            ))
-
-                                        )
-                                }
-
-                </span>
-                        )}
-            </Col>
-
-          </Row>
-
-        </Container>
-      </>
-    )
-
->>>>>>> dev-main
 };
 
 Dashboard.getInitialProps = async (ctx) => {
-<<<<<<< HEAD
   console.log("ctx--", ctx.myCustomData.AuthUserInfo.AuthUser);
   // if (ctx && !ctx.myCustomData.AuthUserInfo.AuthUser) {
   //     ctx.res.writeHead(301, {
@@ -378,52 +218,6 @@ Dashboard.getInitialProps = async (ctx) => {
       let error = new Error(userProfileResponse.statusText);
       error.response = userProfileResponse;
       throw error;
-=======
-
-    console.log('ctx--', ctx.myCustomData.AuthUserInfo.AuthUser)
-    // if (ctx && !ctx.myCustomData.AuthUserInfo.AuthUser) {
-    //     ctx.res.writeHead(301, {
-    //       Location: '/'
-    //     });
-    //     ctx.res.end();
-    //   }
-
-    const userId = ctx.myCustomData.AuthUserInfo.AuthUser.id;
-    console.log('uusseerr', userId)
-    console.log('env', process.env.HOST)
-    // Get profile data
-    const userProfileFetch = fetch(`${process.env.HOST}/api/user?id=${userId}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    });
-
-
-    try {
-        const [userProfileResponse] = await Promise.all([userProfileFetch]);
-
-        if (userProfileResponse.ok) {
-            const { verification } = await userProfileResponse.json();
-            console.log(verification);
-            return {
-                verification
-            }
-        } if ([404, 503].includes(userProfileResponse.status)) {
-            return { statusCode: userProfileResponse.status };
-        } 
-            // https://github.com/developit/unfetch#caveats
-            const error = new Error(userProfileResponse.statusText);
-            error.response = userProfileResponse;
-            throw error;
-        
-
-
-
-    } catch (err) {
-        console.log(err)
-        return {
-            statusCode: (err.response && err.response.status) || err.statusCode || 500,
-        };
->>>>>>> dev-main
     }
   } catch (err) {
     console.log(err);
