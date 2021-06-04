c94bf9dcceca3be17b42728617de9d362aa77b67
Nick Massie <45523540+nimas0@users.noreply.github.com>
2021-06-04 09:02:48 -0700

    testing server error Absolute URLS are only supported 16

--- pages/listing/[listingId]/index.jsx
+++ pages/listing/[listingId]/index.jsx
@@ -353,11 +353,7 @@ const Listing = ({
 Listing.getInitialProps = async (ctx, req) => {
   // Get current listing data from database
   const listingFetch = fetch(
-    `${
-      process.env.NODE_ENV === "development"
-        ? process.env.HOST
-        : process.env.VERCEL_URL
-    }/api/listing?id=${ctx.query.listingId}`,
+    `${process.env.VERCEL_URL}/api/listing?id=${ctx.query.listingId}`,
     {
       method: "GET",
       headers: { "Content-Type": "application/json" },
@@ -366,11 +362,9 @@ Listing.getInitialProps = async (ctx, req) => {
 
   // Get current tour schedules for listing and user
   const tourSchedulesFetch = fetch(
-    `${
-      process.env.NODE_ENV === "development"
-        ? process.env.HOST
-        : process.env.VERCEL_URL
-    }/api/tour-schedules?listingId=${ctx.query.listingId}${
+    `${process.env.VERCEL_URL}/api/tour-schedules?listingId=${
+      ctx.query.listingId
+    }${
       ctx.myCustomData.AuthUserInfo.AuthUser
         ? `&userId=${ctx.myCustomData.AuthUserInfo.AuthUser.id}`
         : ""
