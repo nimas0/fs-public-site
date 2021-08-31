25dc20a329352025b1091ff6a7b645cf9400c856
Nick <nick@findingspaces.com>
2021-08-31 14:37:13 -0700

    add gtm config

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import ErrorPage from 'next/error';
 import '../styles.scss';
@@ -10,33 +10,44 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { ToastProvider, useToasts } from 'react-toast-notifications';
 import 'swiper/swiper-bundle.min.css';
 import 'rc-drawer/assets/index.css';
+import TagManager from "react-gtm-module";
 
 config.autoAddCss = false;
 
+const tagManagerArgs = {
+  id: "GTM-KSKMSHD",
+}
 
-export default ({ Component, pageProps }) => (
-  <>
-    {/* prettier-ignore */}
-    <Head>
-      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+export default ({ Component, pageProps }) => {
+  
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+  
+  
+  return (
+    <>
+      {/* prettier-ignore */}
+      <Head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
 
-      {/* Favicons */}
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
-      {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-32x32.png" /> */}
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="stylesheet" href="/node_modules/owl.carousel/dist/assets/owl.carousel.min.css" />
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
-      />
+        {/* Favicons */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-32x32.png" /> */}
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="stylesheet" href="/node_modules/owl.carousel/dist/assets/owl.carousel.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+        />
 
-      {/* Fonts */}
-      <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700|Roboto:300,400,600,700&display=swap" rel="stylesheet" />
-    </Head>
+        {/* Fonts */}
+        <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700|Roboto:300,400,600,700&display=swap" rel="stylesheet" />
+      </Head>
 
-    {pageProps.statusCode ? (
-      <ErrorPage statusCode={pageProps.statusCode} />
+      {pageProps.statusCode ? (
+        <ErrorPage statusCode={pageProps.statusCode} />
          ) : (
            <>
              <ToastProvider autoDismiss autoDismissTimeout={6000} placement='bottom-right'>
@@ -44,5 +55,6 @@ export default ({ Component, pageProps }) => (
              </ToastProvider>
            </>
          )}
-  </>
-   );
+    </>
+   )
+         };
