7e6ec945df05268722f5e3f0921a4f20f313814d
Nick <nick@findingspaces.com>
2021-09-06 08:55:07 -0700

    fixed compose typo

--- components/buyers/chat/Compose/index.js
+++ components/buyers/chat/Compose/index.js
@@ -3,55 +3,60 @@ import './Compose.css';
 import { useRouter } from 'next/router';
 import firebase from 'firebase/app';
 import 'firebase/database';
-import firebaseInit from '../../../../utils/firebaseInit';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { Form } from 'react-bootstrap';
+import firebaseInit from '../../../../utils/firebaseInit';
 
 // Initialize Firebase app
 firebaseInit();
 
-
-export default function Compose({ messagesEndRef, auth, rightItems, setProposalData }) {
+export default function Compose({
+  messagesEndRef,
+  auth,
+  rightItems,
+  setProposalData,
+}) {
   // get id from url
   const router = useRouter();
-  const interestId = router.query.interestId;
-
-
+  const { interestId } = router.query;
 
   const submitMessage = async (values, { resetForm }) => {
     try {
-      console.log(auth.AuthUser)
+      console.log(auth.AuthUser);
       const { message } = values;
       const { displayName, photoURL, id } = auth.AuthUser;
       // new message entry
-      let newMessage = {
-        displayName: displayName,
-        photoURL: photoURL,
+      const newMessage = {
+        displayName,
+        photoURL,
         author: id,
-        message: message,
-        timestamp: Date.now()
-      }
+        message,
+        timestamp: Date.now(),
+      };
       // Get a key for a new Post.
-      let newPostKey = await firebase.database().ref().child('posts').push().key;
-      let update = {};
+      const newPostKey = await firebase
+        .database()
+        .ref()
+        .child('posts')
+        .push().key;
+      const update = {};
       update[`/interest_chat/${interestId}/${newPostKey}`] = newMessage;
-      console.log(newPostKey)
-      await firebase.database().ref().update(update);
+      console.log(newPostKey);
+      await firebase
+        .database()
+        .ref()
+        .update(update);
       resetForm();
       messagesEndRef.current.scrollIntoView({ behavior: 'auto' });
       // TODO: inefficent calls to firestore to count messages
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
+  };
 
   return (
-    <Formik
-      initialValues={{ message: '' }}
-      onSubmit={submitMessage}>
+    <Formik initialValues={{ message: '' }} onSubmit={submitMessage}>
       {({
         handleSubmit,
         values,
@@ -59,34 +64,26 @@ export default function Compose({ messagesEndRef, auth, rightItems, setProposalD
         handleBlur,
         isSubmitting,
         touched,
-        errors
+        errors,
       }) => (
-          <Form noValidate onSubmit={handleSubmit}>
-
-            <div className="compose">
-
-              <Form.Control
-                autoFocus
-                wrap="soft"
-                name='message'
-                className="compose-input"
-                type='text'
-                value={values.message}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                isInvalid={touched.message && !!errors.message}
-                placeholder="Type a message, @name"
-              />
-              {
-                rightItems
-              }
-            </div>
-
-          </Form>
-        )}
-    </Formik >
+        <Form noValidate onSubmit={handleSubmit}>
+          <div className='compose'>
+            <Form.Control
+              autoFocus
+              wrap='soft'
+              name='message'
+              className='compose-input'
+              type='text'
+              value={values.message}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isInvalid={touched.message && !!errors.message}
+              placeholder='Type a message, @name'
+            />
+            {rightItems}
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
-
-
 }
-
