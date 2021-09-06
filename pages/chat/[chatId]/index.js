d7ce4a8405a8cf0760a15e08a9df09aa281c194d
Nick <nick@findingspaces.com>
2021-09-06 07:37:16 -0700

    added useEffect to push non-auth requests backwards in router history

--- pages/chat/[chatId]/index.js
+++ pages/chat/[chatId]/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from 'next/router';
-// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import withAuthUser from '../../../utils/pageWrappers/withAuthUser';
 import withAuthUserInfo from '../../../utils/pageWrappers/withAuthUserInfo';
 import withLoginModal from '../../../utils/pageWrappers/withLoginModal';
@@ -11,49 +11,56 @@ import ChatComponent from '../../../components/chat-ui/ChatComponent';
 import './standard.css';
 import ChatHeader from '../../../components/chat-ui/ChatHeader';
 import { useMessenger } from '../../../utils/hooks/useMessenger';
-// import 'firebase/database';
-// import firebaseInit from '../../../utils/firebaseInit';
+import 'firebase/database';
+import firebaseInit from '../../../utils/firebaseInit';
 
 // Initialize Firebase app
-// firebaseInit();
+firebaseInit();
 
 const Chat = ({ AuthUserInfo, showLoginModal }) => {
   const { AuthUser = null } = AuthUserInfo;
   const router = useRouter();
+
+  useEffect(() => {
+    if (!AuthUser) {
+      router.back();
+      // todo: add a toast explaining they need to login.
+    }
+  });
 
   // todo: migrate interestId to be called chatId
   // will affect database and seller app
   const { chatId } = router.query;
   const { messages, loading } = useMessenger(chatId);
 
-  // const submitMessage = async (message) => {
-  //   try {
-  //     const { displayName, photoURL, id } = AuthUser;
-  //     // new message entry
-  //     const newMessage = {
-  //       displayName,
-  //       photoURL,
-  //       author: id,
-  //       message,
-  //       timestamp: Date.now(),
-  //     };
-  //     // Get a key for a new Post.
-  //     const newPostKey = firebase
-  //       .database()
-  //       .ref()
-  //       .child('posts')
-  //       .push().key;
-  //     const update = {};
-  //     update[`/interest_chat/${chatId}/${newPostKey}`] = newMessage;
-  //     console.log(newPostKey);
-  //     await firebase
-  //       .database()
-  //       .ref()
-  //       .update(update);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const submitMessage = async (message) => {
+    try {
+      const { displayName, photoURL, id } = AuthUser;
+      // new message entry
+      const newMessage = {
+        displayName,
+        photoURL,
+        author: id,
+        message,
+        timestamp: Date.now(),
+      };
+      // Get a key for a new Post.
+      const newPostKey = firebase
+        .database()
+        .ref()
+        .child('posts')
+        .push().key;
+      const update = {};
+      update[`/interest_chat/${chatId}/${newPostKey}`] = newMessage;
+      console.log(newPostKey);
+      await firebase
+        .database()
+        .ref()
+        .update(update);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   if (loading) return <p>loading</p>;
   return (
@@ -61,13 +68,12 @@ const Chat = ({ AuthUserInfo, showLoginModal }) => {
       <Head>
         <title>Chat with Seller</title>
       </Head>
-      <h6>jsldfjsldfj</h6>
       <ChatHeader AuthUser={AuthUser} />
       <ChatComponent
         messages={messages}
         agentUser={AuthUser.id}
         iconSend={<FontAwesomeIcon icon={faCheck} size={15} />}
-        onMessageSend={"(message) => 'submitMessage(message)'"}
+        onMessageSend={(message) => submitMessage(message)}
         timeFormatter='timeFormatter'
         displayStop
         onMessageStop={() => null}
